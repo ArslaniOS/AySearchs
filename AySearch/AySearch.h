9ea6c07b282eab5c992a2a6274012bdf5e533b1d
Xeven Dev <Arslanyahya700@gmail.com>
2024-08-06 10:51:24 +0500

    Initial Commit

---
+++ AySearch/AySearch.h
@@ -0,0 +1,18 @@
+//
+//  AySearch.h
+//  AySearch
+//
+//  Created by Xeven Dev on 06/08/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AySearch.
+FOUNDATION_EXPORT double AySearchVersionNumber;
+
+//! Project version string for AySearch.
+FOUNDATION_EXPORT const unsigned char AySearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AySearch/PublicHeader.h>
+
+
